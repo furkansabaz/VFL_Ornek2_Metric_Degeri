919ecbe5460a4751d9f7940bf04f245cb540fba1
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-04 16:52:14 +0300

    Initial Commit

---
+++ VFL_Ornek2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VFL_Ornek2
+//
+//  Created by Furkan Sabaz on 4.06.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
